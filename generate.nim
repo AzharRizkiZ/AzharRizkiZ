15a45f7654b6f05146b622cb7c1cf17ded514035
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-04-21 08:13:04 +0700

    Update generate.nim

--- generate.nim
+++ generate.nim
@@ -37,7 +37,7 @@ writeFile("yo.md", &"""<p align="center">
       <img src="metrics.plugin.achivements.svg" alt="Metrics"/>
       <!-- <img src="metrics.plugin.personal.anilist.svg" alt="Metrics"/> -->
       <img src="metrics.plugin.pagespeed.svg" alt="Metrics"/>
-      <img src="metrics.plugin.gists.svg" alt="Metrics"/>
+      <!-- <img src="metrics.plugin.gists.svg" alt="Metrics"/> -->
     </a>
   </p>
 </details>
