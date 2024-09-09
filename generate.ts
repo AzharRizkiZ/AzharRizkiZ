a0d374841caf8823ba84cf464b6052cd8d1ef993
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-09-09 08:36:43 +0700

    Update generate.ts

--- generate.ts
+++ generate.ts
@@ -1,18 +1,40 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+function formatDateTime(date: Date, timeZone: string): string {
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+	      hour12: false, // Use 24-hour time format
+        timeZoneName: 'short',
+        timeZone: timeZone,
+    };
+
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+
 (async () => {
   const readmePath = path.join(__dirname, 'README.md');
   const readmeContents = fs.readFileSync(readmePath, {
     encoding: 'utf-8',
   });
 
-  let [newDate, newTime] = new Date()
-    .toISOString()
-    .replace(/T/, ' ')
+  const utcPlus7 = 'Asia/Jakarta';
+
+  let now = new Date();
+
+  let nowUtcPlus7 = formatDateTime(now, utcPlus7);
+
+  let [newDate, newTime] = nowUtcPlus7
+    .replace(/GMT/, '')
+    .replace(/\+7/, '')
     .replace(/\..+/, '')
     .replace(/-/g, '/')
-    .split(' ');
+    .split(', ');
 
   newDate = newDate.split('/').reverse().join('/');
 
@@ -20,7 +42,7 @@ import * as path from 'path';
 
   const newReadme = readmeContents.replace(
     /Updated: .*/,
-    `Updated: ${newDateTime}`,
+    `Updated: ${newDateTime} Western Indonesia Time`,
   );
 
   fs.writeFileSync(readmePath, newReadme);
